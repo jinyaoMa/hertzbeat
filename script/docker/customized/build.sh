cfce76d4185a600b4500e03d02f95b4a83da44d1
Jinyao Ma <jinyao.ma@outlook.com>
2024-01-03 16:25:22 +0800

    .

--- script/docker/customized/build.sh
+++ script/docker/customized/build.sh
@@ -34,9 +34,9 @@ cp ../../../manager/target/hertzbeat.jar ./hertzbeat.jar
 cp -r ../../../manager/src/main/resources/define/ ./conf/define
 cp -r ../../../web-app/dist/ ./
 
-sed -i "1a\<style type=\"text/css\">global-footer{height:0!important;display:none!important}layout-default-header .alain-default__nav li.ng-star-inserted:nth-child(2),layout-default-header .alain-default__nav li.ng-star-inserted:nth-child(3),layout-default-header .alain-default__nav header-user,layout-default-header .alain-default__header-logo img,layout-basic .alain-default__aside .alain-default__aside-user{display:none!important}.slide.slick-slide nz-tag.ant-tag{font-size:1.2em;line-height:1.3;padding:0.1em 0.5em}a.alain-default__header-logo-link{text-align:left;padding:0 12px;font-size:1.75em;line-height:1}a.alain-default__header-logo-link:before{content:'$1';color:white}.alain-default__collapsed .alain-default__header-logo{width:auto!important}</style>" dist/index.html
-sed -i "4s/HertzBeat/$1/" dist/index.html
-sed -i "3s/HertzBeat/$1/" dist/assets/app-data.json
+sed -i "1a\<style type=\"text/css\">global-footer{height:0\!important;display:none\!important}layout-default-header .alain-default__nav li.ng-star-inserted:nth-child(2),layout-default-header .alain-default__nav li.ng-star-inserted:nth-child(3),layout-default-header .alain-default__nav header-user,layout-default-header .alain-default__header-logo img,layout-basic .alain-default__aside .alain-default__aside-user{display:none\!important}.slide.slick-slide nz-tag.ant-tag{font-size:1.2em;line-height:1.3;padding:0.1em 0.5em}a.alain-default__header-logo-link{text-align:left;padding:0 12px;font-size:1.75em;line-height:1}a.alain-default__header-logo-link:before{content:\"$1\";color:white}.alain-default__collapsed .alain-default__header-logo{width:auto\!important}</style>" dist/index.html
+sed -i "4s/HertzBeat/$(echo $1)/" dist/index.html
+sed -i "3s/HertzBeat/$(echo $1)/" dist/assets/app-data.json
 
 echo "docker build -t hertzbeat:customized -f $CURRENT_DIR/Dockerfile $CONTEXT_DIR"
 
