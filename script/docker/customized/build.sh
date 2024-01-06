bfaa1a8db9daae383f3f1ad16aa074ed93943ef5
Jinyao Ma <jinyao.ma@outlook.com>
2024-01-06 10:32:47 +0800

    .

--- script/docker/customized/build.sh
+++ script/docker/customized/build.sh
@@ -17,9 +17,14 @@
 # limitations under the License.
 
 echo "替换：$1"
+echo "favicon：$2"
 
 cd ../../../web-app
+FROM="router.getCurrentNavigation()?.previousNavigation?.finalUrl.toString() === '/passport/login'"
+TO="router.getCurrentNavigation()?.previousNavigation?.finalUrl.toString() === '/customized'"
+sed -i "s\\$FROM\\$TO\g" src/app/layout/basic/widgets/user.component.ts
 npm run build
+sed -i "s\\$TO\\$FROM\g" src/app/layout/basic/widgets/user.component.ts
 
 cd ../script/docker/customized
 
@@ -34,9 +39,13 @@ cp ../../../manager/target/hertzbeat.jar ./hertzbeat.jar
 cp -r ../../../manager/src/main/resources/define/ ./conf/define
 cp -r ../../../web-app/dist/ ./
 
-sed -i "1a\<style type=\"text/css\">global-footer{height:0\!important;display:none\!important}layout-default-header .alain-default__nav li.ng-star-inserted:nth-child(2),layout-default-header .alain-default__nav li.ng-star-inserted:nth-child(3),layout-default-header .alain-default__nav header-user,layout-default-header .alain-default__header-logo img,layout-basic .alain-default__aside .alain-default__aside-user{display:none\!important}.slide.slick-slide nz-tag.ant-tag{font-size:1.2em;line-height:1.3;padding:0.1em 0.5em}a.alain-default__header-logo-link{text-align:left;padding:0 12px;font-size:1.75em;line-height:1}a.alain-default__header-logo-link:before{content:\"$1\";color:white}.alain-default__collapsed .alain-default__header-logo{width:auto\!important}</style>" dist/index.html
+sed -i "1a\<style type=\"text/css\">global-footer{height:0\!important;display:none\!important}layout-default-header .alain-default__nav li.ng-star-inserted:nth-child(2),layout-default-header .alain-default__nav li.ng-star-inserted:nth-child(3),layout-default-header .alain-default__nav header-user,layout-default-header .alain-default__header-logo img,layout-basic .alain-default__aside .alain-default__aside-user,img[src*='assets/brand'],img[src*='assets/logo']{display:none\!important}.slide.slick-slide nz-tag.ant-tag{font-size:1.2em;line-height:1.3;padding:0.1em 0.5em}a.alain-default__header-logo-link{text-align:left;padding:0 12px;font-size:1.75em;line-height:1}a.alain-default__header-logo-link:before{content:\"$1\";color:white}.alain-default__collapsed .alain-default__header-logo{width:auto\!important}</style>" dist/index.html
 sed -i "4s/HertzBeat/$(echo $1)/" dist/index.html
 sed -i "3s/HertzBeat/$(echo $1)/" dist/assets/app-data.json
+sed -ri "s/hertzbeat/$(echo $1)/Ig" dist/assets/i18n/en-US.json
+sed -ri "s/hertzbeat/$(echo $1)/Ig" dist/assets/i18n/zh-CN.json
+sed -ri "s/hertzbeat/$(echo $1)/Ig" dist/assets/i18n/zh-TW.json
+cp -f $2 dist/favicon.ico
 
 echo "docker build -t hertzbeat:customized -f $CURRENT_DIR/Dockerfile $CONTEXT_DIR"
 
